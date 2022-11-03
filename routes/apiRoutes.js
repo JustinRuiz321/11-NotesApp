878761c95cc8f1f1c73f01521c466d2c4b8dece8
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-11-03 15:38:55 -0400

    Adding comments and cleaning some code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,4 @@
-
+//Constants for the routers
 const router = require('express').Router();
 const fs = require("fs");
 const path = require('path');
@@ -9,11 +9,13 @@ const {
 } = require('../helpers/fsUtils');
 const { v1: uuidv1 } = require('uuid');
 
-
+//To be able to read the notes from the db.json
 router.get("/", (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+
+//To create a new note
 router.post("/", (req,res) => {
     console.log(req.body);
     const { title, text } = req.body;
@@ -29,6 +31,8 @@ router.post("/", (req,res) => {
     }
 });
 
+
+//in order to delete a note that was created
 router.delete('/:id', (req, res) => {
     const noteID = req.params.id;
     readFromFile('./db/db.json')
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
-const index = require('./routes/index')
 const app = express();
+const index = require('./routes/index')
 
+
+//PORT number for local host
+const PORT = process.env.PORT || 3001;
+
+//All app.use lines
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api' , index)
 
+
+//All app.get lines
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
@@ -17,6 +23,7 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
+//Listening when starting the server
 app.listen(PORT, () =>
   console.log(`App listening at localhost:${PORT}`)
 );
