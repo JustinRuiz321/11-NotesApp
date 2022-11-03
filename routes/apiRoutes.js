419670c12e89a0205b4e72f8733a704b33ff1e7a
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-11-03 13:30:49 -0400

    Code works to write new notes. Need to figure out how to delete notes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const fs = require("fs");
+const path = require('path');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
+
+router.get("/", (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+router.post("/", (req,res) => {
+    console.log(req.body);
+    const { title, text } = req.body;
+    if (req.body) {
+      const note = {
+        title,
+        text
+      };
+  
+      readAndAppend(note, './db/db.json');
+      res.json(note);
+    }
+});
+
+module.exports = router;
